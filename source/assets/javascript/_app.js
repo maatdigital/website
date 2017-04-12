faaa6c94be388529ca1bddd32933415441c05692
Daniel Kastl <daniel@georepublic.de>
2017-04-12 15:38:29 +0900

    Adds date/time library

--- source/assets/javascript/_app.js
+++ source/assets/javascript/_app.js
@@ -6,9 +6,67 @@
  */
 var App = (function ($, publ) {
 
+  var VENDOR_CODE = null;
+  var SECRET_KEY = null;
+
   publ.init = function (options) {
     $("pre").append("> App loaded!\n");
+
+    VENDOR_CODE = options.VENDOR_CODE;
+    SECRET_KEY = options.SECRET_KEY;
+
+    $.ajaxSetup({
+      dataType: "json",
+      beforeSend: function (xhr) {
+        var REQUEST_DATE_TIME = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+
+        var params = [];
+        params.push((["code",
+          "'" + VENDOR_CODE + "'"]).join("="));
+        params.push((["date",
+          "'" + REQUEST_DATE_TIME + "'"]).join("="));
+        params.push((["hash",
+          "'" + publ.hashmac(VENDOR_CODE,REQUEST_DATE_TIME) + "'"]).join("="));
+
+        xhr.setRequestHeader('X-Avangate-Authentication', (params).join(" "));
+        console.log((params).join(" "));
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+
+    publ.getProducts();
     return;
+  };
+
+  publ.hashmac = function (code,date) {
+    // TODO: hash generation, see
+    // http://knowledgecenter.avangate.com/Integration/REST_Reference#/introduction/authentication
+    // LEN(VENDOR_CODE) + VENDOR_CODE + LEN(REQUEST_DATE_TIME) + REQUEST_DATE_TIME
+    var hash = code.length + code + date.length + date;
+    $("pre").append("> code: " + code + "\n");
+    $("pre").append("> date: " + date + "\n");
+    $("pre").append("> hash: " + hash + "\n");
+    return hash;
+  };
+
+  publ.getProducts = function (options) {
+    $.ajax({
+      url: "https://api.avangate.com/rest/3.0/products/",
+      type: "GET",
+      data: {
+        "Name": "",
+        "Types": "",
+        "Enabled": "",
+        "GroupName": "",
+        "Limit": "",
+        "Page": ""
+      },
+      success: function (data) {
+        console.log(data);
+      }
+    });
   };
 
   /**
@@ -17,10 +75,3 @@ var App = (function ($, publ) {
   return publ;
 
 })(jQuery, App || {});
-
-/**
- * When DOM is ready, initialize map plugin
- */
-$(document).ready(function(){
-  App.init();
-});
